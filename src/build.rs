da4fc8dcf4408d6a48c39d7f25c28ad7b122b436
takashiidobe <idobetakashi@gmail.com>
2021-10-05 20:57:06 -0500

    fix man page generation

--- src/build.rs
+++ src/build.rs
@@ -3,16 +3,16 @@ use std::fs::File;
 use std::io::{Error, Write};
 
 fn main() -> Result<(), Error> {
-    let path = "rless.1";
+    let path = "rvim.1";
     let mut output = File::create(path)?;
 
-    let msg = Manual::new("rless")
-        .about("A pager in rust.")
+    let msg = Manual::new("rvim")
+        .about("A text editor in rust.")
         .arg(Arg::new("path"))
         .example(
             Example::new()
                 .text("Running the program")
-                .command("rless numbers.txt")
+                .command("rvim [FILE]...")
                 .output("TODO"),
         )
         .author(Author::new("Takashi I").email("mail@takashiidobe.com"))
